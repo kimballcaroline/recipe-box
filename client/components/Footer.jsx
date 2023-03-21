ea5dd9ae0da07ffc469a5ff70a97fbb8eb6e1260
kimballcaroline <kimballcaroline@gmail.com>
2023-03-21 19:27:04 -0400

    Add repo link to footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -7,17 +7,30 @@ import '../stylesheets/footer.scss';
 export default function Footer() {
   return (
     <div className='footer'>
-      <h2>Coded by Caroline Kimball</h2>
-      <div className='icons'>
-        <IconButton
-          href='https://www.linkedin.com/in/kimballcaroline/'
-          target='_blank'
-        >
-          <LinkedInIcon sx={{ color: 'black' }} />
-        </IconButton>
-        <IconButton href='https://github.com/kimballcaroline' target='_blank'>
-          <GitHubIcon sx={{ color: 'black' }} />
-        </IconButton>
+      <div className='col'>
+        <h2>Coded by Caroline Kimball</h2>
+        <div className='icons'>
+          <IconButton
+            href='https://www.linkedin.com/in/kimballcaroline/'
+            target='_blank'
+          >
+            <LinkedInIcon sx={{ color: 'black' }} />
+          </IconButton>
+          <IconButton href='https://github.com/kimballcaroline' target='_blank'>
+            <GitHubIcon sx={{ color: 'black' }} />
+          </IconButton>
+        </div>
+        <div className='col'>
+          <p>
+            This project is open source. You can view the source code&nbsp;
+            <a
+              href='https://github.com/kimballcaroline/recipe-box'
+              target='_blank'
+            >
+              here.
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
