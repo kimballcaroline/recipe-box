9cc42fec27ca4488301e3571cfce436527b92dad
kimballcaroline <kimballcaroline@gmail.com>
2023-03-20 21:55:38 -0400

    Remove excess routes from App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home.jsx";
-import RecipePage from "./components/RecipePage.jsx";
-import AddRecipe from "./components/AddRecipe.jsx";
-import AllRecipes from "./components/AllRecipes.jsx";
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home.jsx';
+import RecipePage from './components/RecipePage.jsx';
+import AddRecipe from './components/AddRecipe.jsx';
+import AllRecipes from './components/AllRecipes.jsx';
+import Footer from './components/Footer.jsx';
+import './stylesheets/styles.scss';
 
 export default function App() {
   return (
-    // // <Router>
-    //   <Home />
-    // // </Router>
-
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/allRecipes" element={<AllRecipes />} />
-      <Route path="/recipe/:id" element={<RecipePage />} />
-      <Route path="/addRecipe" element={<AddRecipe />} />
-    </Routes>
-
-  )
+    <div>
+      <div id='main-content'>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          {/* <Route path='/allRecipes' element={<AllRecipes />} /> */}
+          <Route path='/recipe/:id' element={<RecipePage />} />
+          {/* <Route path='/addRecipe' element={<AddRecipe />} /> */}
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 }
