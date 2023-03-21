619a685f4e577bf5dbff2b3f01c72f78fca1c709
kimballcaroline <kimballcaroline@gmail.com>
2023-03-20 21:58:54 -0400

    Remove excess console.logs

--- client/components/RecipeCard.jsx
+++ client/components/RecipeCard.jsx
@@ -2,33 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../stylesheets/recipeCard.scss';
 
-
 const RecipeCard = (props) => {
- const {
-    title, description, instructions, id, img
-  } = props;
+  const { title, description, instructions, id, img } = props;
 
-  // const navigate = useNavigate();
-  const path = "/recipe/" + id;
-
-  // const handleClick = () => {
-  //   navigate(path);
-  //   navigate(0);
-  // }
+  const path = '/recipe/' + id;
 
   return (
-    <section className="recipeCard">
-      <div className="imgContainer"> <img src={img} alt="illustration of cooking" /></div>
-      <div className="recipeInfoContainer">
-        <h2 className="recipeTitle">{title}</h2>
-        <p className="recipeDescription">{description}</p>
-      </div>
-      <div className="view-btn-wrapper">
-        <Link to={path} className="view-btn">View</Link>
-      </div>
-      
+    <section className='recipeCard'>
+      <h3 className='recipeTitle'>{title}</h3>
+      <p className='recipeDescription'>{description}</p>
+      <Link to={path} className='read-more-link'>
+        Read More
+      </Link>
     </section>
   );
-}
+};
 
 export default RecipeCard;
