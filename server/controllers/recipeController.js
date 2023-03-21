756cdde70b402bdc1845977259e587c93ebbeaa0
kimballcaroline <kimballcaroline@gmail.com>
2023-03-21 19:34:15 -0400

    add findOne query to api/recipes in controller

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -26,7 +26,26 @@ recipeController.getRecipes = (req, res, next) => {
     res.locals.allRecipes = recipes;
     return next();
   });
+
+recipeController.getOneRecipe = (req, res, next) => {
+  const { id } = req.params;
+  Recipe.findOne({ _id: id }, (err, recipe) => {
+    if (err) {
+      return next({
+        log: `The following error occured: ${err}`,
+        status: 418,
+        message: { err: 'An error occured while trying to find a recipe' },
+      });
+    } else if (!recipe) {
+      return next({
+        status: 400,
+        message: { err: 'No recipe with the given id is in the database' },
+      });
 }
+    res.locals.recipe = recipe;
+    return next();
+  });
+};
 
 recipeController.deleteRecipe = (req, res, next) => {
   const { title } = req.body;
