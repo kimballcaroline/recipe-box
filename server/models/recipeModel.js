c71b7aed882fd8a82ccc89698b8b6911b9ec772d
kimballcaroline <kimballcaroline@gmail.com>
2023-03-21 19:44:22 -0400

    remove deprecated strict query from model

--- server/models/recipeModel.js
+++ server/models/recipeModel.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema(
-  {
-    title: { type: String, required: true, unique: true },
-    description: { type: String, required: true },
-    instructions: { type: String, required: true },
-    imageSource: {
-      type: String,
-      default:
-        'https://biritemarket.com/wp-content/uploads/2020/03/FEAST_Recipe_Placeholder_Image-768x576.jpg',
-    },
-    createdDate: { type: Date, default: Date.now },
+const recipeSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  instructions: { type: Schema.Types.Array, required: true },
+  ingredients: { type: Schema.Types.Array, required: true },
+  imageSource: {
+    type: String,
+    default:
+      'https://biritemarket.com/wp-content/uploads/2020/03/FEAST_Recipe_Placeholder_Image-768x576.jpg',
   },
-  { strictQuery: true }
-);
+  createdDate: { type: Date, default: Date.now },
+});
 
 module.exports = mongoose.model('recipe', recipeSchema);
