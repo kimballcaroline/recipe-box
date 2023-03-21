e3862fa27cdb5949c00158bd2231cca86a261122
kimballcaroline <kimballcaroline@gmail.com>
2023-03-21 19:24:23 -0400

    Remove unused routes in App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import RecipePage from './components/RecipePage.jsx';
-import AddRecipe from './components/AddRecipe.jsx';
-import AllRecipes from './components/AllRecipes.jsx';
 import Footer from './components/Footer.jsx';
 import './stylesheets/styles.scss';
 
@@ -13,9 +11,7 @@ export default function App() {
       <div id='main-content'>
         <Routes>
           <Route path='/' exact element={<Home />} />
-          {/* <Route path='/allRecipes' element={<AllRecipes />} /> */}
-          <Route path='/recipe/:id' element={<RecipePage />} />
-          {/* <Route path='/addRecipe' element={<AddRecipe />} /> */}
+          <Route path='/recipe/:_id' element={<RecipePage />} />
         </Routes>
       </div>
       <Footer />
