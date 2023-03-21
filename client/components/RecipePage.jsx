93f4a82311026b3961030edb87428d8492a441b5
kimballcaroline <kimballcaroline@gmail.com>
2023-03-21 19:47:25 -0400

    add ingredient list to recipe page

--- client/components/RecipePage.jsx
+++ client/components/RecipePage.jsx
@@ -1,106 +1,52 @@
-import React, { Component } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../stylesheets/recipePage.scss';
 
-class RecipePage extends Component {
-  constructor(props) {
-    super(props);
+export default function RecipePage() {
+  const { pathname } = window.location;
+  const id = pathname.split('/').pop();
+  const [recipe, setRecipe] = useState({
+    ingredients: [],
+    instructions: [],
+  });
 
-    const { pathname } = window.location;
-    const id = pathname.split('/').pop();
-
-    this.state = {
-      recipesList: {},
-      id: id,
-      recipe: {},
-      fetchedRecipe: false,
-      recipeDeleted: false,
-    };
-
-    // this.addRecipe = this.addRecipe.bind(this);
-    this.deleteRecipe = this.deleteRecipe.bind(this);
-  }
-
-  componentDidMount() {
-    fetch('/api/recipes')
+  useEffect(() => {
+    fetch(`/api/recipes/${id}`)
       .then((res) => res.json())
-      .then((res) => this.addRecipes(res))
-      .catch((err) =>
-        console.log('App.componentDidMount: get recipes ERROR: ', err)
-      );
-  }
+      .then((recipe) => setRecipe(recipe))
+      .catch((err) => console.log('ERROR: Fetching recipe failed', err));
+  }, []);
 
-  addRecipes(recipes) {
-    const recipesList = [...recipes];
-    this.setState({ recipesList });
-    return this.findRecipe(this.state.recipesList);
-  }
+  const ingredientList = recipe.ingredients.map((ingredient, index) => {
+    return <li key={`ingredient${index}`}>{ingredient}</li>;
+  });
 
-  findRecipe(recipes) {
-    for (let i = 0; i < recipes.length; i++) {
-      if (recipes[i]._id === this.state.id) {
-        this.setState({ recipe: { ...recipes[i] } });
-        this.setState({ fetchedRecipe: true });
-      }
-    }
-  }
+  const instructionList = recipe.instructions.map((instruction, index) => {
+    return <li key={`instruction${index}`}>{instruction}</li>;
+  });
 
-  deleteRecipe(event) {
-    const { recipe } = this.state;
-    event.preventDefault();
-    const postPath = '/api/recipes';
-    fetch(postPath, {
-      method: 'DELETE',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify({
-        title: recipe.title,
-      }),
-    })
-      .then((res) => {
-        alert('Congrats! You successfully deleted a recipe to your database!');
-        this.setState({ recipeDeleted: true });
-      })
-      .catch((err) => console.log('RecipePage: delete recipe ERROR: ', err));
-  }
-
-  render() {
-    if (!this.state.fetchedRecipe) return null;
-    if (this.state.recipeDeleted) return <Navigate to='/allRecipes' />;
-    const { recipe } = this.state;
-
-    return (
-      <div className='recipe-page-contents'>
-        <Link to='/' className='home-link'>
-          &#8592; Home
-        </Link>
-        <div className='header'>
-          <img src={recipe.imageSource} alt='' />
-          <div className='title-block'>
-            <h1>{recipe.title}</h1>
-            <p className='recipeDescription'>{recipe.description}</p>
-          </div>
-        </div>
-        <div className='recipe-details'>
-          <div className='ingredients'>
-            <h2>Ingredients</h2>
-            <ul>
-              <li>dfabf</li>
-              <li>dbabf</li>
-              <li>dssgn</li>
-            </ul>
-          </div>
-          <div className='instructions'>
-            <h2>Instructions</h2>
-            <p>{recipe.instructions}</p>
-          </div>
+  return (
+    <div className='recipe-page-contents'>
+      <Link to='/' className='home-link'>
+        &#8592; Home
+      </Link>
+      <div className='header'>
+        <img src={recipe.imageSource} alt='' />
+        <div className='title-block'>
+          <h1>{recipe.title}</h1>
+          <p className='recipeDescription'>{recipe.description}</p>
         </div>
       </div>
-    );
-  }
+      <div className='recipe-details'>
+        <div className='ingredients'>
+          <h2>Ingredients</h2>
+          <ul>{ingredientList}</ul>
+        </div>
+        <div className='instructions'>
+          <h2>Instructions</h2>
+          <ol>{instructionList}</ol>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default RecipePage;
