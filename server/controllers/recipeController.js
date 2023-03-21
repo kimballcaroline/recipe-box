ec2e8d3fd8b15e07b0da05227a40885b2718f8ff
kimballcaroline <kimballcaroline@gmail.com>
2023-03-21 19:33:11 -0400

    improve error handling in addRecipe method

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -3,13 +3,22 @@ const recipeController = {};
 const Recipe = require('../models/recipeModel');
 
 recipeController.addRecipe = (req, res, next) => {
-  const {body} = req;
-  Recipe.create({title: body.title, description: body.description, instructions: body.instructions, imageSource: body.imageSource}, (err, recipe) => {
-    if(err) return next('Error in recipeController.addRecipe: ' + JSON.stringify(err));
-    res.locals.recipe = recipe;
+  const { title, description, instructions, ingredients, imageSource } =
+    req.body;
+  Recipe.create(
+    { title, description, instructions, ingredients, imageSource },
+    (err, recipe) => {
+      if (err)
+        return next({
+          log: `The following error occured: ${err}`,
+          status: 418,
+          message: { err: 'An error occured while trying to create a recipe' },
+        });
+      res.locals.newRecipe = recipe;
     return next();
-  });
 }
+  );
+};
 
 recipeController.getRecipes = (req, res, next) => {
   Recipe.find({}, (err, recipes) => {
