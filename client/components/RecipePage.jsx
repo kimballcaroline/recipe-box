dfad7f2d528114616a4a0089d69a9427ee1f3c44
kimballcaroline <kimballcaroline@gmail.com>
2023-03-20 22:09:15 -0400

    Reformat recipePage header

--- client/components/RecipePage.jsx
+++ client/components/RecipePage.jsx
@@ -59,11 +59,11 @@ class RecipePage extends Component {
       body: JSON.stringify({
         title: recipe.title,
       }),
-      })
-      .then((res) => {
-      alert('Congrats! You successfully deleted a recipe to your database!');
-        this.setState({ recipeDeleted: true });
     })
+      .then((res) => {
+        alert('Congrats! You successfully deleted a recipe to your database!');
+        this.setState({ recipeDeleted: true });
+      })
       .catch((err) => console.log('RecipePage: delete recipe ERROR: ', err));
   }
 
@@ -73,14 +73,17 @@ class RecipePage extends Component {
     const { recipe } = this.state;
 
     return (
-      <div className="recipe-page-contents">
-        <header className="header">
-            <Link to="/allRecipes" className="home-btn">My Recipes</Link>
-            <Link to="/addRecipe" className="add-recipe-btn">Add Recipe</Link>
-        </header>
-        <div className="recipe-info">
-          {/* <h1>Recipe Page</h1> */}
-          <h1>{recipe.title}</h1>
+      <div className='recipe-page-contents'>
+        <Link to='/' className='home-link'>
+          &#8592; Home
+        </Link>
+        <div className='header'>
+          <img src={recipe.imageSource} alt='' />
+          <div className='title-block'>
+            <h1>{recipe.title}</h1>
+            <p className='recipeDescription'>{recipe.description}</p>
+          </div>
+        </div>
         <div className='recipe-details'>
           <div className='ingredients'>
             <h2>Ingredients</h2>
@@ -91,14 +94,13 @@ class RecipePage extends Component {
             </ul>
           </div>
           <div className='instructions'>
-          <h2>Instructions</h2>
+            <h2>Instructions</h2>
             <p>{recipe.instructions}</p>
-        </div>
+          </div>
         </div>
       </div>
     );
   }
 }
-
 
 export default RecipePage;
