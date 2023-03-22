836d66ad114e3d9436949fb6f4b2ea43b28c40b5
kimballcaroline <kimballcaroline@gmail.com>
2023-03-21 20:48:55 -0400

    update endpoints

--- client/components/containers/RecipesContainer.jsx
+++ client/components/containers/RecipesContainer.jsx
@@ -8,7 +8,7 @@ const RecipesContainer = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/recipes')
+    fetch('api/recipes')
       .then((res) => res.json())
       .then((recipes) => {
         setRecipesList(recipes);
